de23f5dd6886d9c0e565cb3045c686e8ed4aa5c4
ymrt43 <ymrt43@gmail.com>
2020-08-23 01:56:13 +0900

    Update to add routing of tasks at routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "groups#index"
+  resources :groups do
+    resources :tasks
+  end
 end
