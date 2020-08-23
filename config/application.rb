54346763d9243451fa5e5b32c0980d65a13a0e9f
ymrt43 <ymrt43@gmail.com>
2020-08-23 22:28:42 +0900

    Set time_zone

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,8 @@ Bundler.require(*Rails.groups)
 module FamilyTies
   class Application < Rails::Application
     config.load_defaults 6.0
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
     config.generators do |g|
       g.stylesheets false
       g.javascripts false
