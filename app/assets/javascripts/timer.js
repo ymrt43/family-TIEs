67bbbc07a2e22e5b2c7c552a63932fb99149cb21
ymrt43 <ymrt43@gmail.com>
2020-08-28 06:44:53 +0900

    Delete  cords with plug-in of timer change

---
+++ app/assets/javascripts/timer.js
@@ -0,0 +1,30 @@
+$(function){
+  $("#DateCountdown").TimeCircles({
+    "animation": "smooth",
+    "bg_width": 1.2,
+    "fg_width": 0.1,
+    "circle_bg_color": "#60686F",
+    "time": {
+        "Days": {
+            "text": "Days",
+            "color": "#FFCC66",
+            "show": true
+        },
+        "Hours": {
+            "text": "Hours",
+            "color": "#99CCFF",
+            "show": true
+        },
+        "Minutes": {
+            "text": "Minutes",
+            "color": "#BBFFBB",
+            "show": true
+        },
+        "Seconds": {
+            "text": "Seconds",
+            "color": "#FF9999",
+            "show": true
+        }
+    }
+});
+}
