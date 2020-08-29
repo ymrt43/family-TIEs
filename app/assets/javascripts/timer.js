db1283abad0876904fe609e6a7b960791f05c894
ymrt43 <ymrt43@gmail.com>
2020-08-29 11:39:28 +0900

    Fix misstaken about definition of jQuery

--- app/assets/javascripts/timer.js
+++ app/assets/javascripts/timer.js
@@ -1,30 +1,42 @@
-$(function){
-  $("#DateCountdown").TimeCircles({
-    "animation": "smooth",
-    "bg_width": 1.2,
-    "fg_width": 0.1,
-    "circle_bg_color": "#60686F",
-    "time": {
-        "Days": {
-            "text": "Days",
-            "color": "#FFCC66",
-            "show": true
-        },
-        "Hours": {
-            "text": "Hours",
-            "color": "#99CCFF",
-            "show": true
-        },
-        "Minutes": {
-            "text": "Minutes",
-            "color": "#BBFFBB",
-            "show": true
-        },
-        "Seconds": {
-            "text": "Seconds",
-            "color": "#FF9999",
-            "show": true
+$(function() {
+    $(".stopwatch").TimeCircles({
+        "animation": "smooth",
+        "bg_width": 0.8,
+        "fg_width": 0.02666666666666667,
+        "circle_bg_color": "#60686F",
+        "start": false,
+        "count_past_zero": false,
+        "time": {
+            "Days": {
+                "text": "Days",
+                "color": "#FFCC66",
+                "show": false
+            },
+            "Hours": {
+                "text": "Hours",
+                "color": "#99CCFF",
+                "show": false
+            },
+            "Minutes": {
+                "text": "Minutes",
+                "color": "#BBFFBB",
+                "show": true
+            },
+            "Seconds": {
+                "text": "Seconds",
+                "color": "#FF9999",
+                "show": true
+            }
         }
-    }
+    });
+    $(".start").click(function(){
+        $(".Tasks__timer--moving.stopwatch").TimeCircles().start();
+    });
+    $(".stop").click(function(){
+        $(".Tasks__timer--moving.stopwatch").TimeCircles().stop();
+    });
+    $(".restart").click(function(){
+        $(".Tasks__timer--moving.stopwatch").TimeCircles().restart();
+    });
 });
-}
+
